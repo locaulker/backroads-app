9cb1b464ee0074e121049ad7dd0e7f02a59fe1df
Lawrence <locaulker@gmail.com>
2023-01-19 00:17:16 -0500

    refactored tours section to react code

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,5 +1,6 @@
 import React from "react"
 import SectionHeader from "./SectionHeader"
+import { tours } from "../data"
 
 const Tours = () => {
   return (
@@ -7,107 +8,54 @@ const Tours = () => {
       <SectionHeader header="featured" subHeader="tours" />
 
       <div className="section-center featured-center">
-        <article className="tour-card">
-          <div className="tour-img-container">
-            <img src="./images/tour-1.jpeg" className="tour-img" alt="" />
-            <p className="tour-date">august 26th, 2020</p>
-          </div>
-          <div className="tour-info">
-            <div className="tour-title">
-              <h4>Tibet Adventure</h4>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-              vitae tempore voluptatum maxime reprehenderit eum quod
-              exercitationem fugit, qui corporis.
-            </p>
-            <div className="tour-footer">
-              <p>
-                <span>
-                  <i className="fas fa-map"></i>
-                </span>{" "}
-                china
-              </p>
-              <p>6 days</p>
-              <p>from $2100</p>
-            </div>
-          </div>
-        </article>
+        {tours.map(tour => {
+          const {
+            id,
+            image,
+            date,
+            heading,
+            info,
+            icon,
+            location,
+            duration,
+            cost
+          } = tour
 
-        <article className="tour-card">
-          <div className="tour-img-container">
-            <img src="./images/tour-2.jpeg" className="tour-img" alt="" />
-            <p className="tour-date">october 1st, 2020</p>
-          </div>
-          <div className="tour-info">
-            <h4>best of java</h4>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-              vitae tempore voluptatum maxime reprehenderit eum quod
-              exercitationem fugit, qui corporis.
-            </p>
-            <div className="tour-footer">
-              <p>
-                <span>
-                  <i className="fas fa-map"></i>
-                </span>{" "}
-                indonesia
-              </p>
-              <p>11 days</p>
-              <p>from $1400</p>
-            </div>
-          </div>
-        </article>
+          // This snippent is to format the cost price 'cost'
+          const tourCost = (
+            <span>
+              {new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD"
+              }).format(cost)}
+            </span>
+          )
 
-        <article className="tour-card">
-          <div className="tour-img-container">
-            <img src="./images/tour-3.jpeg" className="tour-img" alt="" />
-            <p className="tour-date">september 15th, 2020</p>
-          </div>
-          <div className="tour-info">
-            <h4>explore hong kong</h4>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-              vitae tempore voluptatum maxime reprehenderit eum quod
-              exercitationem fugit, qui corporis.
-            </p>
-            <div className="tour-footer">
-              <p>
-                <span>
-                  <i className="fas fa-map"></i>
-                </span>{" "}
-                hong kong
-              </p>
-              <p>8 days</p>
-              <p>from $5000</p>
-            </div>
-          </div>
-        </article>
-
-        <article className="tour-card">
-          <div className="tour-img-container">
-            <img src="./images/tour-4.jpeg" className="tour-img" alt="" />
-            <p className="tour-date">december 5th, 2019</p>
-          </div>
-          <div className="tour-info">
-            <h4>kenya highlights</h4>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-              vitae tempore voluptatum maxime reprehenderit eum quod
-              exercitationem fugit, qui corporis.
-            </p>
-            <div className="tour-footer">
-              <p>
-                <span>
-                  <i className="fas fa-map"></i>
-                </span>{" "}
-                kenya
-              </p>
-              <p>20 days</p>
-              <p>from $3300</p>
-            </div>
-          </div>
-        </article>
+          return (
+            <article key={id} className="tour-card">
+              <div className="tour-img-container">
+                <img src={image} className="tour-img" alt={heading} />
+                <p className="tour-date">{date}</p>
+              </div>
+              <div className="tour-info">
+                <div className="tour-title">
+                  <h4>{heading}</h4>
+                </div>
+                <p>{info}</p>
+                <div className="tour-footer">
+                  <p>
+                    <span>
+                      <i className={icon}></i>
+                    </span>{" "}
+                    {location}
+                  </p>
+                  <p>{duration} days</p>
+                  <p>from {tourCost}</p>
+                </div>
+              </div>
+            </article>
+          )
+        })}
       </div>
     </section>
   )
--- src/data.js
+++ src/data.js
@@ -9,13 +9,13 @@ export const pageLinks = [
   { id: 1, href: "#home", text: "home" },
   { id: 2, href: "#about", text: "about" },
   { id: 3, href: "#services", text: "services" },
-  { id: 4, href: "#tours", text: "tours" },
+  { id: 4, href: "#tours", text: "tours" }
 ]
 
 export const socialLinks = [
   { id: 1, url: "https://www.facebook.com", icon: "fab fa-facebook" },
   { id: 2, url: "https://www.twitter.com", icon: "fab fa-twitter" },
-  { id: 3, url: "https://www.squarespace.com", icon: "fab fa-squarespace" },
+  { id: 3, url: "https://www.squarespace.com", icon: "fab fa-squarespace" }
 ]
 
 export const services = [
@@ -23,20 +23,20 @@ export const services = [
     id: 1,
     icon: "fas fa-wallet fa-fw",
     heading: "saving money",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia."
   },
   {
     id: 2,
     icon: "fas fa-tree fa-fw",
     heading: "endless hiking",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia."
   },
   {
     id: 3,
     icon: "fas fa-socks fa-fw",
     heading: "amazing comfort",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
-  },
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia."
+  }
 ]
 
 export const tours = [
@@ -49,61 +49,61 @@ export const tours = [
     icon: "fas fa-map",
     location: "china",
     duration: 6,
-    cost: "2100",
+    cost: "2100"
   },
   {
     id: 2,
     image: tour2Img,
     date: "october 1st, 2024",
-    heading: "Tibet Adventure",
+    heading: "indonesia Adventure",
     info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
     icon: "fas fa-map",
     location: "indonesia",
-    duration: 6,
-    cost: "2100",
+    duration: 4,
+    cost: "1400"
   },
   {
     id: 3,
     image: tour3Img,
     date: "august 26th, 2024",
-    heading: "Tibet Adventure",
+    heading: "asia Adventure",
     info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
     icon: "fas fa-map",
     location: "hong kong",
-    duration: 6,
-    cost: "2100",
+    duration: 8,
+    cost: "1900"
   },
   {
     id: 4,
     image: tour4Img,
     date: "september 15th, 2024",
-    heading: "Tibet Adventure",
+    heading: "japan Adventure",
     info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
     icon: "fas fa-map",
-    location: "china",
-    duration: 6,
-    cost: "2100",
+    location: "japan",
+    duration: 7,
+    cost: "3100"
   },
   {
     id: 5,
     image: tour5Img,
     date: "november 26th, 2024",
-    heading: "Tibet Adventure",
+    heading: "canada Adventure",
     info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
     icon: "fas fa-map",
-    location: "china",
-    duration: 6,
-    cost: "2100",
+    location: "canada",
+    duration: 14,
+    cost: "4100"
   },
   {
     id: 6,
     image: tour6Img,
     date: "december 26th, 2024",
-    heading: "Tibet Adventure",
+    heading: "Turkish Adventure",
     info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
     icon: "fas fa-map",
-    location: "china",
-    duration: 6,
-    cost: "2100",
-  },
+    location: "turkey",
+    duration: 21,
+    cost: "5000"
+  }
 ]
