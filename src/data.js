c9c44e318989ba616e9df4e3ff3310066b26a378
Lawrence Caulker <locaulker@gmail.com>
2023-09-10 09:37:07 -0400

    refactored services section

--- src/data.js
+++ src/data.js
@@ -10,3 +10,24 @@ export const socialLinks = [
   { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
   { id: 3, href: "https://www.squarespace.com", icon: "fab fa-squarespace" }
 ]
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Chronic heart failure (CHF) - a pathological condition of the body, allocated in a separate disease accompanied by deficiency of blood supply."
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: "Most developers have to spend quite a lot of time and effort on building customers understanding of the application and disseminating information about it."
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    text: "Intel Company reported on the next achievement on the way from traditional electronic circuits to OPTICAL CHANNELS transfer of information inside computers."
+  }
+]
