55529a4068ba348f0fc82c67518c92fed72e744e
Lawrence Caulker <locaulker@gmail.com>
2023-09-10 16:54:56 -0400

    refactored tour section from static to dynamic

--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import tour1 from "./images/tour-1.jpeg"
+import tour2 from "./images/tour-2.jpeg"
+import tour3 from "./images/tour-3.jpeg"
+import tour4 from "./images/tour-4.jpeg"
+
 export const pageLinks = [
   { id: 1, href: "#home", text: "home" },
   { id: 2, href: "#about", text: "about" },
@@ -29,5 +34,48 @@ export const services = [
     icon: "fas fa-socks fa-fw",
     title: "amazing comfort",
     text: "Intel Company reported on the next achievement on the way from traditional electronic circuits to OPTICAL CHANNELS transfer of information inside computers."
+  }
+]
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: "August 26th, 2024",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "China",
+    duration: 6,
+    price: 2100
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: "October 1th, 2024",
+    title: "Best of Java",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "Indonesia",
+    duration: 11,
+    price: 1400
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: "September 15th, 2024",
+    title: "Explore Hong Kong",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "Hong Kong",
+    duration: 8,
+    price: 5000
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: "December 5th, 2023",
+    title: "Kenya Highlights",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "Kenya",
+    duration: 20,
+    price: 3300
   }
 ]
