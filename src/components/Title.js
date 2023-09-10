074c138c0fc68c9829edf2a4743eb49e5a3335c7
Lawrence Caulker <locaulker@gmail.com>
2023-09-10 08:25:37 -0400

    implemented Title component for section Headings

---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+const Title = ({ pT1, pT2 }) => {
+  return (
+    <div className="section-title">
+      <h2>
+        {pT1} <span>{pT2}</span>
+      </h2>
+    </div>
+  )
+}
+export default Title
